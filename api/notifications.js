855c63fe5f5f8a7fea01cfbcc6c3b0b080a80c8e
ghana <ghana.chindukuri@gmail.com>
2021-04-08 18:13:15 +0530

    deployment ready version

--- api/notifications.js
+++ api/notifications.js
@@ -1,42 +1,39 @@
-/** @format */
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
+const NotificationModel = require("../models/NotificationModel");
+const UserModel = require("../models/UserModel");
 
-const express = require('express')
-const router = express.Router()
-const authMiddleware = require('../middleware/authMiddleware')
-const NotificationModel = require('../models/NotificationModel')
-const UserModel = require('../models/UserModel')
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+    const { userId } = req;
 
-router.get('/', authMiddleware, async (req, res) => {
-	try {
-		const {userId} = req
+    const user = await NotificationModel.findOne({ user: userId })
+      .populate("notifications.user")
+      .populate("notifications.post");
 
-		const user = await NotificationModel.findOne({user: userId})
-			.populate('notifications.user')
-			.populate('notifications.post')
-		console.log('user: ', user)
+    return res.json(user.notifications);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server Error");
+  }
+});
 
-		return res.json(user.notifications)
-	} catch (error) {
-		console.error(error)
-		return res.status(500).send('Server Error')
-	}
-})
+router.post("/", authMiddleware, async (req, res) => {
+  try {
+    const { userId } = req;
 
-router.post('/', authMiddleware, async (req, res) => {
-	try {
-		const {userId} = req
+    const user = await UserModel.findById(userId);
 
-		const user = await UserModel.findById(userId)
+    if (user.unreadNotification) {
+      user.unreadNotification = false;
+      await user.save();
+    }
+    return res.status(200).send("Updated");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server Error");
+  }
+});
 
-		if (user.unreadNotification) {
-			user.unreadNotification = false
-			await user.save()
-		}
-		return res.status(200).send('Updated')
-	} catch (error) {
-		console.error(error)
-		return res.status(500).send('Server Error')
-	}
-})
-
-module.exports = router
+module.exports = router;
