ede8f778de1bfd60e0d9ce083976fa52935e0703
Chindukuri Pavan <ghana.chindukuri@gmail.com>
2021-05-20 15:42:08 +0530

    removing "/"

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -3,6 +3,6 @@
 const baseUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'http://localhost:3000'
-		: 'https://mini-social-media.vercel.app/'
+		: 'https://mini-social-media.vercel.app'
 
 module.exports = baseUrl
