cd6ebb0876533b95e739065b588ba7b73ce796b9
Chindukuri Pavan <ghana.chindukuri@gmail.com>
2021-05-20 15:59:51 +0530

    Vercel -> Heroku

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -3,6 +3,6 @@
 const baseUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'http://localhost:3000'
-		: 'https://mini-social-media.vercel.app'
+		: 'https://mini-social-media-7989.herokuapp.com'
 
 module.exports = baseUrl
