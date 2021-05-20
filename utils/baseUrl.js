947a22e98301ed0a5d9938a379e09d827227678b
Chindukuri Pavan <ghana.chindukuri@gmail.com>
2021-05-20 15:37:17 +0530

    updating baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -3,6 +3,6 @@
 const baseUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'http://localhost:3000'
-		: 'https://mini-social-media-getnrvpm1-ghana7989.vercel.app'
+		: 'https://mini-social-media.vercel.app/'
 
 module.exports = baseUrl
