3079b2a7a8d3418901f123061034309017eafd8d
Chindukuri Pavan <ghana.chindukuri@gmail.com>
2021-05-20 15:28:48 +0530

    updating baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,8 @@
-const baseUrl =
-  process.env.NODE_ENV !== "production"
-    ? "http://localhost:3000"
-    : "https://inder-social-media2.herokuapp.com";
+/** @format */
 
-module.exports = baseUrl;
+const baseUrl =
+	process.env.NODE_ENV !== 'production'
+		? 'http://localhost:3000'
+		: 'https://mini-social-media-getnrvpm1-ghana7989.vercel.app'
+
+module.exports = baseUrl
